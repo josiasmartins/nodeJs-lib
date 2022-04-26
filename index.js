2d6f37a89b0c90eb740d85e7041d83e911782a40
Caitano <josiasmartinscaitano@gmail.com>
2022-04-26 15:14:22 -0300

    fixture: adiconado a lib fs - nativa do nodeJs

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import chalk from 'chalk';
+// fs(file system): biblioteca nativa do nodejs
+import fs from 'fs';
 
 console.log(chalk.blue("vamos começar!"));
 
@@ -8,4 +10,14 @@ function texto(string) {
     return string;  
 }
 
-console.log(texto(paragrafo));
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8'
+    // _: o javascript ignora o parametro do método
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+        console.log(chalk.green(texto));
+    });
+}
+
+// console.log(texto(paragrafo));
+pegaArquivo('./arquivos/texto1.md')
+// console.log(pegaArquivo('./arquivos/texto1.md'));
