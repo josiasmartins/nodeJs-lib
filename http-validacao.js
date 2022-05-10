45510078dbcf270a9e67ed9c843526d6a3d8a85b
Caitano <josiasmartinscaitano@gmail.com>
2022-05-10 08:08:25 -0300

    feat: validando links

--- http-validacao.js
+++ http-validacao.js
@@ -1,4 +1,11 @@
+export function geraArrayDeURLs(arraylinks) {
+    // loop para cada { chave: valor }
+    // Object.values: pega o objeto e retorna somente o valor em arry
+    // objeto -> [valor]
+    return arraylinks.map(objetoLink => Object.values(objetoLink).join())
+}
+
 export default function validaURLs(arraylinks) {
-    return arraylinks;
+    return geraArrayDeURLs(arraylinks);
 }
 
